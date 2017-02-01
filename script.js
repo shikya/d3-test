c58c17a345a11aef3970e0f3fab714e9bca20e45
Shrikant Sonone <shrikant.sonone@coda.global>
2017-02-01 17:44:41 +0530

    Bar chart

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-var orangedata = [10, 30 ,50 ,100];
+
+var width = 200,
+    height = 200,
+    data = [10, 15, 50, 56, 140, 150, 200, 220]
+
+console.log(d3);
+
+var scaling = d3.scaleLinear()
+    .domain([0, 220])
+    .range([0, width]);
 
 var canvas = d3
-    .select('.orangecontainers')
+    .select('.barchart')
     .append('svg')
-    .attr('width',1024).attr('height',768);
+    .attr('width',width).attr('height',height);
 
-var orange = canvas
-    .selectAll('circle')
-    .data(orangedata)
-    .enter()
-        .append('circle')
-        .attr('fill','orange')
-        .attr('cx',function(d, i){
-            return d+ i*100;
-        })
-        .attr('cy',function(d,i){
-            return d
-        })
-        .attr('r',function(d, i){
-            return d;
-        })
+var chart = canvas
+        .selectAll('rec')
+        .data(data)
+        .enter()
+            .append('rect')
+            .attr('x',0)
+            .attr('y',function(d, i){
+                return 10+ i*20
+            })
+            .attr('width',function(d, i){
+                // return d;
+                return scaling(d);
+            })
+            .attr('height',10)
+            .attr('fill','red')
